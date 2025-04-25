f3de8406bb3f38c40f569e8ae90d91a58eb8a515
aseef <contact@aseef.dev>
2025-04-25 17:53:55 -0400

    Added more ducknet configs for test

--- configs/__init__.py
+++ configs/__init__.py
@@ -1,10 +1,12 @@
 from .ducknet_34_config import DuckNet34Config
 from .ducknet_32_config import DuckNet32Config
 from .ducknet_17_config import DuckNet17Config
+from .ducknet_8_config import DuckNet8Config
 from .slim_ducknet_config import SlimDuckNetConfig
 from .kd_slim_ducknet_config import KDSlimDuckNetConfig
 from .kd_hot_slim_ducknet_config import KDHotSlimDuckNetConfig
 from .us_ducknet_config import USSlimDuckNetConfig
+from .optimized_slim_ducknet_config import OptimizedSlimDuckNetConfig
 from .optuna_config import OptunaConfig
 from .config_registry import config_hub
 #Unfreeze Segmentation Head
---
+++ configs/ducknet_8_config.py
@@ -0,0 +1,53 @@
+import os.path
+
+from .base_config import BaseConfig
+from .config_registry import register_config
+
+
+@register_config
+class DuckNet8Config(BaseConfig):
+
+    def __init__(self,):
+        super().__init__()
+        # Change Save Dir for different configs!
+        self.save_dir = 'save/ducknet_8'
+
+        # Dataset
+        self.dataset = 'polyp'
+        self.subset = 'kvasir'
+        self.data_root = os.path.join('PolypDataset', 'Kvasir-SEG')
+        self.use_test_set = True
+
+        # Model
+        self.model = 'ducknet'
+        self.base_channel = 8
+
+        # Training
+        self.total_epoch = 600
+        self.train_bs = 16  # this is PER GPU
+        self.loss_type = 'dice'
+        self.optimizer_type = 'rmsprop'
+        self.base_lr = 1e-4
+
+        # Validating
+        self.metrics = ['dice', 'iou']
+        self.val_bs = 1
+
+        # Training setting
+        self.use_ema = False
+        self.logger_name = 'medseg_trainer'
+
+        # Augmentation
+        self.crop_size = 320
+        self.randscale = None
+        self.brightness = [0.6, 1.6]
+        self.contrast = 0.2
+        self.saturation = 0.1
+        self.h_flip = 0.5
+        self.v_flip = 0.5
+        self.norm_mean = None
+        self.norm_std = None
+        self.affine_shear = (-22.5, 22)
+        self.affine_rotate = (-180, 180)
+        self.affine_translate = (-0.125, 0.125)
+        self.affine_scale = (0.5, 1.5)
